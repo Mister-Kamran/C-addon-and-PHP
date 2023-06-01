e9049333e729dad78ee6c4fb762efdb475f78a94
Mister-Kamran <135120735+Mister-Kamran@users.noreply.github.com>
2023-06-01 10:25:34 +0400

    Create php_api.cs

---
+++ php_api.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHP_Api
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Hello PHP");
+
+        }
+    }
+}
